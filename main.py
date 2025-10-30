aac8559a8ccaeb59a2d5a09028ad39e47549111d
Daniel <daniel.fer333@gmail.com>
2025-10-30 19:41:49 +0100

    Add gitignore, .env. and requirements

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
 import os
 
 # 🔑 Sustituye por tu token
-TOKEN = os.environ["BOT_TOKEN"]
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("¡Hola! Soy tu bot. 😊")
+    await update.message.reply_text("¡Hola! Soy tu bot en dev pero en la rama de git!! 😊")
 
 async def ayuda(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Comandos disponibles:\n/start - saludar\n/frase - obtener una frase motivadora")
@@ -25,5 +28,5 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler(["ayuda","help"], ayuda))
 app.add_handler(CommandHandler("frase", frase))
 
-print("Bot en marcha... Ctrl+C para detenerlo...")
+print("Bot en marcha... Ctrl+C para detenerlo.")
 app.run_polling()
